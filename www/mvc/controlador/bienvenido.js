7cb1f85613553bcbcbeac42506590a44127b9f15
Santyy <santiagovillanueva702@gmail.com>
2024-10-16 19:28:50 -0300

    Modelado en base al Figma

---
+++ www/mvc/controlador/bienvenido.js
@@ -0,0 +1,20 @@
+function toggleForms() {
+    const loginForm = document.getElementById("loginForm");
+    const registerForm = document.getElementById("registerForm");
+
+    if (loginForm.style.display === "none") {
+        loginForm.style.display = "block";
+        registerForm.style.display = "none";
+    } else {
+        loginForm.style.display = "none";
+        registerForm.style.display = "block";
+    }
+}
+
+function login() {
+    alert("Iniciar sesión");
+}
+
+function register() {
+    alert("Registrarse");
+}
